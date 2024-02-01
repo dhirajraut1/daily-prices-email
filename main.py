20d57ad1dca3c253ce96201cdbc8c0bb707271d7
Dhiraj Raut <rautdhiraj277@gmail.com>
2024-02-01 12:40:09 +0545

    update style

--- main.py
+++ main.py
@@ -102,25 +102,25 @@ html_content = f"""
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Prices Nepal</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    </head>
-    <style>
-    td, th {
-        border: 1px solid #ddd;
-        padding: 8px;
-    }
-    tr:nth-child(even){
-        background-color: #f2f2f2;
-    }
+        <style>
+            td, th {
+                border: 1px solid #ddd;
+                padding: 8px;
+            }
+            tr:nth-child(even){
+                background-color: #f2f2f2;
+            }
 
-    th {
-      padding-top: 12px;
-      padding-bottom: 12px;
-      text-align: left;
-      background-color: #04AA6D;
-      color: white;
-    }
-        
+            th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+            }
     </style>
+    </head>
+    
     <body>
         <img src="https://raw.githubusercontent.com/dhirajraut1/daily-prices-email/main/priceNepalLogo.png" class="img-fluid img-thumbnail rounded mx-auto d-block" width="80px" alt="Price Nepal Logo">
         <h2>Prices for {today_date}</h2>
