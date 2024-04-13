a19780c72bf61673d6e2accc53b153b7f6148f1e
Dhiraj Raut <rautdhiraj277@gmail.com>
2024-04-13 18:23:43 +0545

    Feature: Create post for Gold prices

--- create_image_post.py
+++ create_image_post.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import requests
 import datetime
+import nepali_datetime
 from pyppeteer import launch
 from dotenv import load_dotenv
 
@@ -11,18 +12,22 @@ access_token = os.getenv('ACCESS_TOKEN')
 page_id = os.getenv('PAGE_ID')
 base_url = f'https://graph.facebook.com/{page_id}/photos'
 fuel_website = "https://dhirajraut27.com.np/fuel-prices/"
+gold_website = "https://www.fenegosida.org/rate-history.php"
+fuel_website_element = "#fuel-price-img"
+gold_website_element = ".rate-content"
 
 # Function to capture screenshot
-async def capture_screenshot():
+async def capture_screenshot(url, element_id, filename):
     browser = await launch(headless=True)
     page = await browser.newPage()
-    await page.goto(fuel_website)
+    await page.setViewport({'width': 1280, 'height': 720})
+    await page.goto(url)
 
-    element = await page.querySelector('#fuel-price-img')
+    element = await page.querySelector(element_id)
     bounding_box = await element.boundingBox()
 
     await page.screenshot({
-        'path': 'fuel-img.png',
+        'path': f'{filename}.png',
         'clip': {
             'x': bounding_box['x'],
             'y': bounding_box['y'],
@@ -33,30 +38,36 @@ async def capture_screenshot():
     await browser.close()
 
 # Capture screenshot
-asyncio.get_event_loop().run_until_complete(capture_screenshot())
+asyncio.get_event_loop().run_until_complete(capture_screenshot(fuel_website,fuel_website_element, 'fuel'))
+asyncio.get_event_loop().run_until_complete(capture_screenshot(gold_website,gold_website_element, 'gold'))
 
-current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-message = f'Fuel Price for {current_date}'
-image_path = 'example1.png'
+# current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+nepali_date = nepali_datetime.date.today()
+message = f'Price for {nepali_date}'
+image_paths = ['fuel.png', 'gold.png']
 # image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fuel.png')
 
 
+for image_path in image_paths:
 # Create a file object
-with open(image_path, 'rb') as image_file:
-    # Parameters for the post
-    params = {
-        'access_token': access_token,
-        'message': message,
-    }
-    
-    # Create a dictionary for files
-    files = {'source': image_file}
-    
-    # Make a POST request to create the post
-    response = requests.post(base_url, data=params, files=files)
+    try:
+        with open(image_path, 'rb') as image_file:
+        # Parameters for the post
+            params = {
+                'access_token': access_token,
+                'message': message,
+            }
+            
+            # Create a dictionary for files
+            files = {'source': image_file}
+            
+            # Make a POST request to create the post
+            response = requests.post(base_url, data=params, files=files)
 
-# Check if the request was successful
-if response.status_code == 200:
-    print("Post created successfully!")
-else:
-    print("Error:", response)
+        # Check if the request was successful
+        if response.status_code == 200:
+            print("Post created successfully!")
+        else:
+            print("Error:", response)
+    except Exception as e:
+        print(f"Error uploading image {image_path}: {e}")
